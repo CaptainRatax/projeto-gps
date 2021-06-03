66a52fededdb852aa096afe766e1cc8c4df42aba
IIIRataxIII <56645921+IIIRataxIII@users.noreply.github.com>
2021-06-03 23:25:22 +0100

    Ajuste no Dummy
    Foi alterada a lógica do dummy por causa de um problema que estava a dar em alguns requests seguidos ao mesmo.

--- routes/api.js
+++ routes/api.js
@@ -6,13 +6,14 @@ var candidaturas = require('./apiRoutes/candidaturas')
 
 router.use('/candidaturas', candidaturas)
 
+const dummySchemma = new mongoose.Schema({
+    dummy: 'string',
+    dummy2: 'string'
+})
+const Dummy = mongoose.model('Dummy', dummySchemma)
+
 //Request Dummy que vai ser chamado de tanto em tanto tempo para não deixar o heroku e o mongodb entrarem em repouso
 router.get('/dummy', async(req, res) => {
-    const dummySchemma = new mongoose.Schema({
-        dummy: 'string',
-        dummy2: 'string'
-    })
-    const Dummy = mongoose.model('Dummy', dummySchemma)
     try {
         var dummy = new Dummy({ "dummy": "dummy", "dummy2": "dummy2" })
         var dummyGuardado = await dummy.save()
