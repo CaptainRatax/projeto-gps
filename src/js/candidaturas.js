d094480c1b1dc9b60753e8c23b97aaeb6b2ccabc
jcarmo1998 <58702386+jcarmo1998@users.noreply.github.com>
2021-06-24 22:32:28 +0100

    Página das inscrições dinâmica
    Já dá para fazer inscrição e escolher um dos politécnicos disponíveis.

--- src/js/candidaturas.js
+++ src/js/candidaturas.js
@@ -1,12 +1,61 @@
+var apiBaseUrl;
+
 $(() => {
+    getAPIUrl();
     getCandidaturasAbertas();
+    $("#enviar").click(() => {
+        submeterCandidatura();
+    })
 })
 
+async function getAPIUrl() {
+    var file = "../../apiBaseUrl.txt"
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function() {
+        if (rawFile.readyState === 4) {
+            if (rawFile.status === 200 || rawFile.status == 0) {
+                apiBaseUrl = rawFile.responseText;
+            }
+        }
+    }
+    rawFile.send(null);
+}
+
 function getCandidaturasAbertas() {
-    $.get("http://localhost:3000/api/universidades/candidaturasAbertas", (data) => {
-        if (data.length == 0) {
+    $.get(apiBaseUrl + "/universidades/candidaturasAbertas", (politecnicos) => {
+        if (politecnicos.length == 0) {
             $('#exampleModal').modal('show');
+        } else {
+            politecnicos.forEach(politecnico => {
+                $("#politecnicos").append('<option value="' + politecnico.sigla + '">' + politecnico.nome + '</option>');
+            });
         }
     })
+}
 
+function submeterCandidatura() {
+    var sigla = $('select[name=selector] option').filter(':selected').val();
+    $.get(apiBaseUrl + "/universidades/sigla/" + sigla, (politecnico) => {
+        var candidatura = {
+            nome: $("#inputNome").val(),
+            email: $("#inputEmail").val(),
+            telemovel: $("#inputTelemovel").val(),
+            escola: $("#inputEscola").val(),
+            descricao: $("#textAreaDescricao").val(),
+            imagem: "",
+            aprovacao: null,
+            universidade: politecnico
+        }
+
+        console.log(JSON.stringify(candidatura));
+
+        $.ajax({
+            type: 'POST',
+            url: apiBaseUrl + '/candidaturas/nova',
+            data: JSON.stringify(candidatura),
+            processData: false,
+            contentType: 'application/json',
+        });
+    })
 }
