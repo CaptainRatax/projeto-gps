7d7a9359f069c13b1b814fd58d13de9e37d86409
jcarmo1998 <58702386+jcarmo1998@users.noreply.github.com>
2021-07-03 16:04:56 +0100

    Pop-up's na página DadosSite
    Adicionado pop-up com mensagem de sucesso ou erro na criação.
    Adicionado pop-up para confirmar a alteração dos dados.

--- src/js/dadosSite.js
+++ src/js/dadosSite.js
@@ -44,66 +44,86 @@ function getDadosSite() {
 }
 
 function updateDadosSite() {
+    var erro = false;
+    var confirmacao = confirm("Tem a certeza que quer alterar os dados do site?");
+    if (confirmacao == true) {
+        var chaveSubtituloHome = $("#labelSubtituloHome").text()
+        $.get(apiBaseUrl + "/dadossite/chave/" + chaveSubtituloHome, (data) => {
+            data.valor = $("#subtituloHome").val()
+            console.log(JSON.stringify(data))
+            $.ajax({
+                type: 'PATCH',
+                url: apiBaseUrl + '/dadossite/alterar',
+                data: JSON.stringify(data),
+                processData: false,
+                contentType: 'application/json',
+                error: function(err) {
+                    erro = true;
+                },
+                /* success and error handling omitted for brevity */
+            });
+        })
 
-    var chaveSubtituloHome = $("#labelSubtituloHome").text()
-    $.get(apiBaseUrl + "/dadossite/chave/" + chaveSubtituloHome, (data) => {
-        data.valor = $("#subtituloHome").val()
-        console.log(JSON.stringify(data))
-        $.ajax({
-            type: 'PATCH',
-            url: apiBaseUrl + '/dadossite/alterar',
-            data: JSON.stringify(data),
-            processData: false,
-            contentType: 'application/json',
+        var chaveTituloSobre = $("#labelTituloSobre").text()
+        $.get(apiBaseUrl + "/dadossite/chave/" + chaveTituloSobre, (data) => {
+            data.valor = $("#tituloSobre").val()
+            console.log(JSON.stringify(data))
+            $.ajax({
+                type: 'PATCH',
+                url: apiBaseUrl + '/dadossite/alterar',
+                data: JSON.stringify(data),
+                processData: false,
+                contentType: 'application/json',
+                error: function(err) {
+                    erro = true;
+                },
+                /* success and error handling omitted for brevity */
+            });
+        })
 
-            /* success and error handling omitted for brevity */
-        });
-    })
+        var chaveTituloHome = $("#labelTituloHome").text()
+        $.get(apiBaseUrl + "/dadossite/chave/" + chaveTituloHome, (data) => {
+            data.valor = $("#tituloHome").val()
+            console.log(JSON.stringify(data))
+            $.ajax({
+                type: 'PATCH',
+                url: apiBaseUrl + '/dadossite/alterar',
+                data: JSON.stringify(data),
+                processData: false,
+                contentType: 'application/json',
+                error: function(err) {
+                    erro = true;
+                },
+                /* success and error handling omitted for brevity */
+            });
+        })
 
-    var chaveTituloSobre = $("#labelTituloSobre").text()
-    $.get(apiBaseUrl + "/dadossite/chave/" + chaveTituloSobre, (data) => {
-        data.valor = $("#tituloSobre").val()
-        console.log(JSON.stringify(data))
-        $.ajax({
-            type: 'PATCH',
-            url: apiBaseUrl + '/dadossite/alterar',
-            data: JSON.stringify(data),
-            processData: false,
-            contentType: 'application/json',
-
-            /* success and error handling omitted for brevity */
-        });
-    })
-
-    var chaveTituloHome = $("#labelTituloHome").text()
-    $.get(apiBaseUrl + "/dadossite/chave/" + chaveTituloHome, (data) => {
-        data.valor = $("#tituloHome").val()
-        console.log(JSON.stringify(data))
-        $.ajax({
-            type: 'PATCH',
-            url: apiBaseUrl + '/dadossite/alterar',
-            data: JSON.stringify(data),
-            processData: false,
-            contentType: 'application/json',
-
-            /* success and error handling omitted for brevity */
-        });
-    })
-
-    var chaveTextoSobre = $("#labelTextoAreaSobre").text()
-    $.get(apiBaseUrl + "/dadossite/chave/" + chaveTextoSobre, (data) => {
-        data.valor = $("#textoAreaSobre").val()
-        console.log(JSON.stringify(data))
-        $.ajax({
-            type: 'PATCH',
-            url: apiBaseUrl + '/dadossite/alterar',
-            data: JSON.stringify(data),
-            processData: false,
-            contentType: 'application/json',
-
-            /* success and error handling omitted for brevity */
-        });
-    })
-
-
+        var chaveTextoSobre = $("#labelTextoAreaSobre").text()
+        $.get(apiBaseUrl + "/dadossite/chave/" + chaveTextoSobre, (data) => {
+            data.valor = $("#textoAreaSobre").val()
+            console.log(JSON.stringify(data))
+            $.ajax({
+                type: 'PATCH',
+                url: apiBaseUrl + '/dadossite/alterar',
+                data: JSON.stringify(data),
+                processData: false,
+                contentType: 'application/json',
+                error: function(err) {
+                    erro = true;
+                },
+                /* success and error handling omitted for brevity */
+            });
+        })
+        if (erro) {
+            $(".error").empty();
+            $(".error").append("Ocurreu algum erro ao guardar os dados. Por favor tente mais tarde.");
+            $(".success").css("display", "none");
+            $(".error").css("display", "block");
+        } else {
+            $(".success").empty();
+            $(".success").append("Dados guardados com sucesso!");
+            $(".success").css("display", "block");
+            $(".error").css("display", "none");
+        }
+    }
 }
