24c70e49c4ff250a6da4d443590ee6d5a53d348b
IIIRataxIII <56645921+IIIRataxIII@users.noreply.github.com>
2021-07-04 20:52:16 +0100

    Colocar a página de contactos a apresentar dados dinâmicos e mapa verdadeiro
    Editar a página dos contactos para apresentar os dados da base de dados.
    Editar a página dos contactos para que apresente um mapa verdadeiro e funcional como por exemplo o da página de edição dos contactos.

---
+++ src/js/contactos.js
@@ -0,0 +1,98 @@
+var apiBaseUrl;
+var globalPolitecnicos = [];
+
+$(() => {
+    getAPIUrl();
+    getUniversidades();
+})
+
+function getAPIUrl() {
+    var file = "../../apiBaseUrl.txt"
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function() {
+        if (rawFile.readyState === 4) {
+            if (rawFile.status === 200 || rawFile.status == 0) {
+                apiBaseUrl = rawFile.responseText;
+            }
+        }
+    }
+    rawFile.send(null);
+}
+
+function getUniversidades() {
+    $.get(apiBaseUrl + "/universidades", (politecnicos) => {
+        globalPolitecnicos = {};
+        politecnicos.forEach(politecnico => {
+            globalPolitecnicos[politecnico.sigla] = politecnico._id;
+            $("#politecnicos").append('<option value="' + politecnico.sigla + '">' + politecnico.nome + '</option>');
+        });
+        getContactos(politecnicos[0]._id);
+    })
+}
+
+function onUnivesityChange() {
+    var sigla = $('select[name=selector] option').filter(':selected').val()
+    getContactos(globalPolitecnicos[sigla]);
+}
+
+function getContactos(idUniversidade) {
+    if (!idUniversidade) {
+        if (globalPolitecnicos.length == 0) {
+            return;
+        }
+        var sigla = $('select[name=selector] option').filter(':selected').val()
+        idUniversidade = globalPolitecnicos[sigla]
+    }
+    $.get(apiBaseUrl + "/contactos/morada/universidadeid/" + idUniversidade, (morada) => {
+        var auxData = morada.valor.replaceAll("\n", "<br>")
+        $("#moradaContactos").text("")
+        $("#moradaContactos").append(auxData)
+        $.get(apiBaseUrl + "/contactos/universidadeid/" + idUniversidade, (contactos) => {
+            let contador = 0;
+            globalContactos = [];
+            $("#contactoCampos").empty();
+            contactos.forEach(contacto => {
+                if (contacto.ordem != 0) {
+                    $("#contactoCampos").append(
+                        '<div id="itemContacto' + contador + '" class="itemContacto">' +
+                        '<div id="tituloItemContacto' + contador + '" class="tituloItemContacto">' +
+                        '<span id="chave' + contador + '">' + contacto.chave + '</span>' +
+                        '</div>' +
+                        '<div id="valorItemContacto' + contador + '" class="valorItemContacto">' +
+                        '<span id="valor' + contador + '">' + contacto.valor + '</span>' +
+                        '</div>' +
+                        '</div>'
+                    )
+                    globalContactos[contador] = contacto;
+                    contador += 1;
+                }
+            });
+        })
+        myMap();
+    })
+}
+
+function myMap() {
+    if (globalPolitecnicos.length != 0) {
+        var sigla = $('select[name=selector] option').filter(':selected').val();
+        $.get(apiBaseUrl + "/contactos/coordenadas/universidadeid/" + globalPolitecnicos[sigla], (data) => {
+            var auxCoordinates = data.valor.split(", ");
+            var coordinates;
+            if (!isNaN(auxCoordinates[0]) && !isNaN(auxCoordinates[1])) {
+                coordinates = { lat: parseFloat(auxCoordinates[0]), lng: parseFloat(auxCoordinates[1]) };
+            } else {
+                coordinates = { lat: 40.64654781049706, lng: -7.92019846053499 };
+            }
+            const map = new google.maps.Map(document.getElementById("googleMap"), {
+                zoom: 17,
+                center: coordinates,
+            });
+
+            var marker = new google.maps.Marker({
+                position: coordinates,
+                map: map,
+            });
+        })
+    }
+}
