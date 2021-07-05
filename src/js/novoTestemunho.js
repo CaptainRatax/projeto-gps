6d1a81b89729152601eed34407da56cb4e3fa490
jcarmo1998 <58702386+jcarmo1998@users.noreply.github.com>
2021-07-05 22:54:10 +0100

    Adicionar e Editar Testemunhos
    Já dá para adicionar e editar os testemunhos.

--- src/js/novoTestemunho.js
+++ src/js/novoTestemunho.js
@@ -1,7 +1,9 @@
 var apiBaseUrl;
+var testemunho;
+
 $(() => {
     getAPIUrl();
-    //getTestemunhos();
+    getTestemunhos();
     getUniversidades();
 })
 
@@ -28,50 +30,126 @@ function getUniversidades() {
 }
 
 function guardar() {
-    if (localStorage.getItem("novoTestemunho") != "") { // EDITAR TESTEMUNHO
+    if ($("#Estudante").val() == "" || $("#Descricao").val() == "" || $("#YoutubeLink").val() == "") {
+        $(".success").alert('close')
+        $(".error").alert('close')
+        $(".warning").alert('close')
+        $("#alerts").append(
+            '<div class="warning alert alert-warning alert-dismissible fade fixed" role="alert">' +
+            'Vá lá... Preenche os dados por favor! 😒😐' +
+            '<button type="button" class="close" data-dismiss="alert" aria-label="Close">' +
+            '<span aria-hidden="true">&times;</span>' +
+            '</button>' +
+            '</div>'
+        );
+        $(".warning").addClass("show");
+    } else {
+        if (localStorage.getItem("novoTestemunho") != "") { // EDITAR TESTEMUNHO
+            var sigla = $('select[name=selector] option').filter(':selected').val()
+            $.get(apiBaseUrl + "/universidades/sigla/" + sigla, (universidade) => {
+                testemunho.nomePessoa = $("#Estudante").val()
+                testemunho.descricao = $("#Descricao").val()
+                testemunho.linkYoutube = $("#YoutubeLink").val()
+                testemunho.universidade = universidade
+                $.ajax({
+                    type: 'PATCH',
+                    url: apiBaseUrl + '/testemunhos/alterar',
+                    data: JSON.stringify(testemunho),
+                    processData: false,
+                    contentType: 'application/json',
+                    success: function() {
+                        getTestemunhos();
+                        $(".success").alert('close')
+                        $(".error").alert('close')
+                        $(".warning").alert('close')
+                        $("#alerts").append(
+                            '<div class="success alert alert-success alert-dismissible fade fixed" role="alert">' +
+                            'Testemunho alterado com sucesso!' +
+                            '<button type="button" class="close" data-dismiss="alert" aria-label="Close">' +
+                            '<span aria-hidden="true">&times;</span>' +
+                            '</button>' +
+                            '</div>'
+                        );
+                        $(".success").addClass("show");
+                    },
+                    error: function(err) {
+                        $(".success").alert('close')
+                        $(".error").alert('close')
+                        $(".warning").alert('close')
+                        $("#alerts").append(
+                            '<div class="error alert alert-danger alert-dismissible fade fixed" role="alert">' +
+                            'Ocurreu algum erro a tentar guardar o testemunho. Por favor tente mais tarde.' +
+                            '<button type="button" class="close" data-dismiss="alert" aria-label="Close">' +
+                            '<span aria-hidden="true">&times;</span>' +
+                            '</button>' +
+                            '</div>'
+                        );
+                        $(".error").addClass("show");
+                    }
+                });
+            })
 
-    } else { // ADICIONAR TESTEMUNHO
-        var today = new Date();
-        var dd = String(today.getDate()).padStart(2, '0');
-        var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-        var yyyy = today.getFullYear();
+        } else { // ADICIONAR TESTEMUNHO
+            var today = new Date();
+            var dd = String(today.getDate()).padStart(2, '0');
+            var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+            var yyyy = today.getFullYear();
 
-        today = mm + '/' + dd + '/' + yyyy;
-        var sigla = $('select[name=selector] option').filter(':selected').val();
-        $.get(apiBaseUrl + "/universidades/sigla/" + sigla, (universidade) => {
-            var testemunho = {
-                nomePessoa: $("#Estudante").val(),
-                descricao: $("#Descricao").val(),
-                data: today,
-                linkYoutube: $("#YoutubeLink").val(),
-                universidade: universidade
-            }
-
-            $.ajax({
-                type: 'POST',
-                url: apiBaseUrl + '/testemunhos/novo',
-                data: JSON.stringify(testemunho),
-                processData: false,
-                contentType: 'application/json',
-                success: function() {
-                    $(".success").css("display", "block");
-                    $(".error").css("display", "none");
-                    $(".warning").css("display", "none");
-                },
-                error: function() {
-                    $(".error").css("display", "block");
-                    $(".warning").css("display", "none");
-                    $(".success").css("display", "none");
+            today = mm + '/' + dd + '/' + yyyy;
+            var sigla = $('select[name=selector] option').filter(':selected').val();
+            $.get(apiBaseUrl + "/universidades/sigla/" + sigla, (universidade) => {
+                var testemunho = {
+                    nomePessoa: $("#Estudante").val(),
+                    descricao: $("#Descricao").val(),
+                    data: today,
+                    linkYoutube: $("#YoutubeLink").val(),
+                    universidade: universidade
                 }
-            });
-        })
+
+                $.ajax({
+                    type: 'POST',
+                    url: apiBaseUrl + '/testemunhos/novo',
+                    data: JSON.stringify(testemunho),
+                    processData: false,
+                    contentType: 'application/json',
+                    success: function() {
+                        $(".success").css("display", "block");
+                        $(".error").css("display", "none");
+                        $(".warning").css("display", "none");
+                    },
+                    error: function() {
+                        $(".error").css("display", "block");
+                        $(".warning").css("display", "none");
+                        $(".success").css("display", "none");
+                    }
+                });
+            })
+        }
     }
 }
 
 function getTestemunhos() {
     if (localStorage.getItem("novoTestemunho") != "") { // EDITAR TESTEMUNHO
+        $("#titulo").text("Editar Testemunho");
         $.get(apiBaseUrl + '/testemunhos', (data) => {
-
+            procurarTestemunho();
         })
+    } else {
+        $("#titulo").text("Novo Testemunho");
     }
+}
+
+function cancelar() {
+    window.location.href = "/administracao/testemunhos";
+}
+
+function procurarTestemunho() {
+    let id = localStorage.getItem("novoTestemunho");
+    $.get(apiBaseUrl + '/testemunhos/id/' + id, (data) => {
+        $("#Estudante").val(data.nomePessoa);
+        $("#YoutubeLink").val(data.linkYoutube);
+        $("#Descricao").val(data.descricao);
+        $("#politecnicos").val(data.universidade.sigla).change();
+        testemunho = data;
+    })
 }
