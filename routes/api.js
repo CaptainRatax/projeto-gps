a8fc4e08e729d0598b8f5ce710cb20a951b39781
IIIRataxIII <56645921+IIIRataxIII@users.noreply.github.com>
2021-07-06 02:19:16 +0100

    (On Going...) Request de login para a área de adminsitração
    ### Feito:
     - Request de login no backend que devolve um token e o utilizador no caso de dar sucesso;
     - Request de verificar token, para verificar se o token não expirou ou é válido;
     - Página de login já envia os dados para a api e só redireciona no caso de um pedido com sucesso;
     - Página de login apresenta os devidos alertas no caso de não dar sucesso;
    
    ### Falta:
     - Assim que o pedido de login for success guardar o token e o utilizador devolvidos em local storage, ou cockies.
     - Colocar em todas as páginas de administração uma lógica para verificar o token que está em local storage, ou cockies, para que se o token for inválido, redireciona para o login.

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,12 @@
 var express = require('express')
 var router = express.Router()
 var mongoose = require('mongoose')
+require('dotenv').config()
+var emailValidator = require("email-validator");
+var jwt = require('jsonwebtoken');
+
+const { Utilizador } = require('../models/utilizadorSchema')
+const { Login } = require('../models/loginSchema')
 
 var candidaturas = require('./apiRoutes/candidaturas')
 var universidades = require('./apiRoutes/universidades')
@@ -19,6 +25,87 @@ router.use('/utilizadores', utilizadores)
 router.use('/dadossite', dadosSite)
 router.use('/testemunhos', testemunhos)
 router.use('/faqs', faqs)
+
+//Login
+
+router.post('/login', async(req, res) => {
+    try {
+        var loginInfo = new Login(req.body);
+        if (!loginInfo.email) {
+            return res.status(400).send('Corpo do pedido inválido')
+        }
+        if (!loginInfo.password) {
+            return res.status(400).send('Corpo do pedido inválido')
+        }
+        if (!emailValidator.validate(loginInfo.email)) {
+            return res.status(400).send('Email inválido')
+        }
+
+        var confirmacaoUser = Utilizador.find({ email: loginInfo.email, password: loginInfo.password }, function(err, results) {
+            if (err) {
+                res.status(500).send("Algo correu mal com o pedido")
+                console.log('Pedido POST de login falhou! Erros:')
+                return console.error(err)
+            }
+            if (!results) {
+                console.log('Pedido POST de login recebido e não autorizado com sucesso')
+                return res.status(401).send('Email ou Password errados!');
+            } else if (results.length == 1) {
+                var token = jwt.sign({
+                    exp: Math.floor(Date.now() / 1000) + (60 * 60),
+                    data: loginInfo.email + ':' + loginInfo.password
+                }, process.env.TOKEN_SECRET);
+                var response = {
+                    user: results[0],
+                    token: token
+                }
+                res.status(200).send(response);
+                console.log('Pedido POST de login recebido e autorizado com sucesso')
+            } else {
+                console.log('Pedido POST de login recebido e não autorizado com sucesso')
+                return res.status(401).send('Email ou Password errados!');
+            }
+        })
+    } catch (error) {
+        res.status(500).send("Algo correu mal com o pedido")
+        console.log('Pedido POST de login falhou! Erros:')
+        return console.error(error)
+    }
+})
+
+router.post('/verifyToken', async(req, res) => {
+    try {
+        if (!req.body.token) {
+            return res.status(400).send('Corpo do pedido inválido')
+        }
+        jwt.verify(req.body.token, process.env.TOKEN_SECRET, function(err, decoded) {
+            if (!decoded) {
+                res.status(401).send("Token inválido! Não autorizado!")
+                console.log('Pedido POST de verificar token recebido e não autorizado com sucesso')
+            } else {
+                var userInfo = decoded.data.split(":");
+                var confirmacaoUser = Utilizador.find({ email: userInfo[0], password: userInfo[1] }, function(err, results) {
+                    if (err) {
+                        res.status(500).send("Algo correu mal com o pedido")
+                        console.log('Pedido POST de verificar token falhou! Erros:')
+                        return console.error(err)
+                    }
+                    if (!results) {
+                        console.log('Pedido POST de verificar token recebido e não autorizado com sucesso')
+                        return res.status(401).send('Email ou Password errados!');
+                    } else {
+                        res.status(200).send({ success: true });
+                        console.log('Pedido POST de verificar token recebido e autorizado com sucesso')
+                    }
+                })
+            }
+        });
+    } catch (error) {
+        res.status(500).send("Algo correu mal com o pedido")
+        console.log('Pedido POST de verificar token falhou! Erros:')
+        return console.error(error)
+    }
+})
 
 const dummySchemma = new mongoose.Schema({
     dummy: 'string',
